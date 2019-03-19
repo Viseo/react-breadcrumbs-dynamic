299341ccc9e6ee9f5da256bfa739d04aa6e17678
Julien AVERT <julien.avert@viseo.com>
2019-03-19 11:55:36 +0100

    lib refacto

--- src/index.js
+++ src/index.js
@@ -1,104 +1,91 @@
-import React, { Children } from 'react'
-import PropTypes from 'prop-types'
+import { append, compose, identity, ifElse, init, intersperse, isEmpty, last, length, map, sort, values } from 'ramda';
+import React from 'react';
+import { createAdvAgent, throughAgent, throughContainer, throughInterface, ThroughProvider } from 'react-through';
 
-import {
-  ThroughProvider,
-  throughContainer,
-  throughAgent,
-  createAdvAgent,
-  throughInterface,
-} from 'react-through'
+export const breadcrumbsThroughArea = 'breadcrumbs';
+export const breadcrumbsBearingKey = 'to';
 
+export const withBreadcrumbs = throughInterface(breadcrumbsThroughArea);
+export const withBreadcrumbsItem = throughAgent(breadcrumbsThroughArea, breadcrumbsBearingKey);
+export const withBreadcrumbsContainer = throughContainer(breadcrumbsThroughArea);
 
-export const breadcrumbsThroughArea = 'breadcrumbs'
+export const Dummy = () => null;
+export const Item = () => null;
 
-export const breadcrumbsBearingKey = 'to'
-
-export const withBreadcrumbs = throughInterface(breadcrumbsThroughArea)
-
-export const withBreadcrumbsItem = throughAgent(breadcrumbsThroughArea, breadcrumbsBearingKey)
-
-export const withBreadcrumbsContainer = throughContainer(breadcrumbsThroughArea)
-
-export const Dummy = () => null
-
-export const Item = () => null
-
-export const BreadcrumbsProvider = ThroughProvider
-
-export const BreadcrumbsItem = createAdvAgent(breadcrumbsThroughArea, breadcrumbsBearingKey)
-
+export const BreadcrumbsProvider = ThroughProvider;
+export const BreadcrumbsItem = createAdvAgent(breadcrumbsThroughArea, breadcrumbsBearingKey);
 
 function prepareProps(props, rename, duplicate, remove) {
-  const p = Object.assign({}, props)
+  const p = Object.assign({}, props);
   Object.keys(duplicate).forEach(k => {
-    p[duplicate[k]] = p[k]
-  })
+    p[duplicate[k]] = p[k];
+  });
   Object.keys(rename).forEach(k => {
-    p[rename[k]] = p[k]; delete p[k]
-  })
+    p[rename[k]] = p[k];
+    delete p[k];
+  });
   Object.keys(remove).forEach(k => {
-    delete p[k]
-  })
-  return p
+    delete p[k];
+  });
+  return p;
 }
 
-const defaultCompare = (a, b) => (
-  a[breadcrumbsBearingKey].length - b[breadcrumbsBearingKey].length
-)
+const defaultCompare = (a, b) => a[breadcrumbsBearingKey].length - b[breadcrumbsBearingKey].length;
 
-const Breadcrumbs_ = (props) => {
-  const {
-    container: Container = 'span',
-    containerProps,
-    hideIfEmpty = false,
-    item: Item = 'a',
-    finalItem: FinalItem = Item,
-    finalProps = {},
-    separator,
-    duplicateProps: duplicate = {},
-    removeProps: remove  = {},
-    renameProps: rename = (Item === 'a' ? {to: 'href'} : {}),
-    compare
-  } = props
-  const data = props[breadcrumbsThroughArea]
-  const itemsValue = Object
-    .keys(data)
-    .map(k => data[k])
-    .sort(compare || defaultCompare)
-  const count = itemsValue.length
+const Breadcrumbs_ = ({
+  [breadcrumbsThroughArea]: data = {},
 
-  if (hideIfEmpty && count === 0) {
-    return null
+  hideIfEmpty = false,
+
+  compare = defaultCompare,
+
+  container: Container = 'span',
+  containerProps = {},
+  item: Item = 'a',
+  finalItem: FinalItem = Item,
+  finalProps = {},
+  separator = '/',
+
+  duplicateProps: duplicate = {},
+  removeProps: remove = {},
+  renameProps: rename = Item === 'a' ? { to: 'href' } : {},
+}) => {
+  const breadcrumbsData = values(data);
+
+  if (hideIfEmpty && breadcrumbsData.length === 0) {
+    return null;
   }
+
+  const separatorElement = <span className="separator">{separator}</span>;
+
+  const sortedBreadcrumbsData = sort(compare, breadcrumbsData);
+
+  const startItems = compose(
+    ifElse(isEmpty, identity, append(separatorElement)),
+    intersperse(separatorElement),
+    map(item => (
+      <span className="item">
+        <Item {...prepareProps(item, rename, duplicate, remove)} />
+      </span>
+    )),
+    init
+  )(sortedBreadcrumbsData);
+
+  const lastData = last(sortedBreadcrumbsData);
+  const lastItem = lastData ? (
+    <span /*key={length(startItems)}*/ className="item item--final">
+      <FinalItem {...prepareProps(lastData, rename, duplicate, remove)} {...finalProps} />
+    </span>
+  ) : null;
 
   return (
     <Container {...containerProps}>
-
-      {itemsValue.map((itemValue, i) => {
-        return i+1 < count ? (
-
-          separator ? (
-            <span key={i}>
-              <Item {...prepareProps(itemValue, rename, duplicate, remove)} />
-              {separator}
-            </span>
-          ) : (
-            <Item key={i} {...prepareProps(itemValue, rename, duplicate, remove)} />
-          )
-
-        ) : (
-
-          <FinalItem key={i}
-            {...prepareProps(itemValue, rename, duplicate, remove)}
-            {...finalProps}
-          />
-
-        )
-      })}
-
+      {startItems.map((Item, i) => (
+        <Item.type key={i} {...Item.props} />
+      ))}
+      {lastItem}
     </Container>
-  )
-}
+  );
+};
 
-export const Breadcrumbs = withBreadcrumbsContainer(Breadcrumbs_)
+export const Breadcrumbs = withBreadcrumbsContainer(Breadcrumbs_);
